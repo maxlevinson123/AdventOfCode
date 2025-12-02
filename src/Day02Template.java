ff27758333becd49a3c10076a35bfb8757375d9f
Max <maxl77@nycstudents.net>
2025-12-02 16:55:10 -0500

    a

---
+++ src/Day02Template.java
@@ -0,0 +1,61 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day02Template {
+    public static void main(String[] args) {
+
+        // Day 2 data parsing set up
+        ArrayList<String> fileData = getFileData("src/data");
+        ArrayList<Long> productIDs = new ArrayList<Long>();
+        for (String s : fileData.get(0).split(",")) {
+            long first = Long.parseLong(s.split("-")[0]);
+            long second = Long.parseLong(s.split("-")[1]);
+            for (long i = first; i <= second; i++) {
+                productIDs.add(i);
+            }
+        }
+        // Day 2 data parsing set up complete
+        // "productIDs" is a list of "long" variables that make up each product ID from the file
+
+        long partOneAnswer = 0;
+
+        for (Long p : productIDs) {
+            if (isValidProductID(p)) {
+                partOneAnswer += p;
+            }
+
+        }
+
+        System.out.println("Part one answer: " + partOneAnswer);
+    }
+
+    public static boolean isValidProductID(long productID) {
+        String pID = productID + "";
+        if (productID %2 == 0) {
+            if ((pID.substring(0, pID.length()/2)).equals(pID.substring(pID.length()/2))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static ArrayList<String> getFileData(String fileName) {
+        ArrayList<String> fileData = new ArrayList<String>();
+        try {
+            File f = new File(fileName);
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                String line = s.nextLine();
+                if (!line.equals(""))
+                    fileData.add(line);
+            }
+            return fileData;
+        }
+        catch (FileNotFoundException e) {
+            return fileData;
+        }
+    }
+}
