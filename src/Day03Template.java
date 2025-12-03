e197e1261093d272c0ce3e9f31362805ea39e9ab
MaxLevinson <maxl77@nycstudents.net>
2025-12-03 08:46:29 -0500

    a

---
+++ src/Day03Template.java
@@ -0,0 +1,45 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day03Template {
+    public static void main(String[] args) {
+
+        // **** DO NOT EDIT ANYTHING HERE ****
+        ArrayList<String> fileData = getFileData("src/data");
+        int partOneAnswer = 0;
+        for (String batteries : fileData) {
+            int voltage = getLargestCombination(batteries);
+            partOneAnswer += voltage;
+        }
+
+        System.out.println("Part one answer: " + partOneAnswer);
+    }
+
+    // COMPLETE THIS METHOD!
+    public static int getLargestCombination(String batteries) {
+        String firstDigit = "0";
+        for (int i = 0; i < batteries.length(); i++) {
+            if (Integer.parseInt(batteries.substring(i, i + 1)) > firstDigit)
+        }
+    }
+
+
+    public static ArrayList<String> getFileData(String fileName) {
+        ArrayList<String> fileData = new ArrayList<String>();
+        try {
+            File f = new File(fileName);
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                String line = s.nextLine();
+                if (!line.equals(""))
+                    fileData.add(line);
+            }
+            return fileData;
+        }
+        catch (FileNotFoundException e) {
+            return fileData;
+        }
+    }
+}
