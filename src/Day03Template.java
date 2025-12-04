a151ef88f051d99edb5833491765b817bceb996c
MaxLevinson <maxl77@nycstudents.net>
2025-12-04 08:46:17 -0500

    a

--- src/Day03Template.java
+++ src/Day03Template.java
@@ -19,10 +19,27 @@ public class Day03Template {
 
     // COMPLETE THIS METHOD!
     public static int getLargestCombination(String batteries) {
-        String firstDigit = "0";
+        int firstDigit = 0;
+        int firstDigitPlace = 0;
+        int secondDigit = 0;
+        int secondDigitPlace = 0;
         for (int i = 0; i < batteries.length(); i++) {
-            if (Integer.parseInt(batteries.substring(i, i + 1)) > firstDigit)
+            if (Integer.parseInt(batteries.substring(i, i + 1)) > firstDigit) {
+                firstDigit = Integer.parseInt(batteries.substring(i, i + 1));
+                firstDigitPlace = i;
+            }
         }
+        String newString = batteries.substring(0,firstDigitPlace) + batteries.substring(firstDigitPlace + 1);
+        for (int i = 0; i < newString.length(); i++) {
+            if (Integer.parseInt(newString.substring(i, i + 1)) > firstDigit) {
+                secondDigit = Integer.parseInt(newString.substring(i, i + 1));
+                secondDigitPlace = i;
+            }
+        }
+        if (firstDigitPlace <= secondDigitPlace) {
+            return Integer.parseInt((firstDigit + "") + (secondDigit + ""));
+        }
+        return Integer.parseInt((secondDigit + "") + (firstDigitPlace + ""));
     }
 
 
